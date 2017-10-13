9de27a0453d8f07b031b1add4b71516b40f15a24
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-10-13 02:56:02 -0500

    add backoff hint for too busy error (#203)

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,14 @@ extern crate futures;
 extern crate grpcio;
 
 pub mod coprocessor;
-pub mod debugpb_grpc;
 pub mod debugpb;
+pub mod debugpb_grpc;
 pub mod eraftpb;
 pub mod errorpb;
 pub mod kvrpcpb;
 pub mod metapb;
-pub mod pdpb_grpc;
 pub mod pdpb;
+pub mod pdpb_grpc;
 pub mod raft_cmdpb;
 pub mod raft_serverpb;
 pub mod tikvpb_grpc;
