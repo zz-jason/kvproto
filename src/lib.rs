97053ac5f197bd7996a8e638c9e37d50324a5665
Shirly <AndreMouche@126.com>
2018-01-03 12:04:12 +0800

    init runtime details for coprocessor (#217)

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@ extern crate futures;
 extern crate grpcio;
 
 pub mod coprocessor;
-pub mod debugpb_grpc;
 pub mod debugpb;
+pub mod debugpb_grpc;
 pub mod eraftpb;
 pub mod errorpb;
 pub mod kvrpcpb;
 pub mod metapb;
-pub mod pdpb_grpc;
 pub mod pdpb;
+pub mod pdpb_grpc;
 pub mod raft_cmdpb;
 pub mod raft_serverpb;
-pub mod tikvpb_grpc;
 pub mod tikvpb;
+pub mod tikvpb_grpc;
