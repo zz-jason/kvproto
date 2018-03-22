16057ebc249da4da94b6256389dd43b224db6a0b
UncP <uncp.xu@gmail.com>
2018-03-22 14:23:53 +0800

    add modify tikv config api

--- src/debugpb_grpc.rs
+++ src/debugpb_grpc.rs
@@ -95,6 +95,13 @@ const METHOD_DEBUG_CHECK_REGION_CONSISTENCY: ::grpcio::Method<super::debugpb::Re
     resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
 };
 
+const METHOD_DEBUG_MODIFY_TIKV_CONFIG: ::grpcio::Method<super::debugpb::ModifyTikvConfigRequest, super::debugpb::ModifyTikvConfigResponse> = ::grpcio::Method {
+    ty: ::grpcio::MethodType::Unary,
+    name: "/debugpb.Debug/ModifyTikvConfig",
+    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+};
+
 pub struct DebugClient {
     client: ::grpcio::Client,
 }
@@ -273,6 +280,22 @@ impl DebugClient {
     pub fn check_region_consistency_async(&self, req: &super::debugpb::RegionConsistencyCheckRequest) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::debugpb::RegionConsistencyCheckResponse>> {
         self.check_region_consistency_async_opt(req, ::grpcio::CallOption::default())
     }
+
+    pub fn modify_tikv_config_opt(&self, req: &super::debugpb::ModifyTikvConfigRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::debugpb::ModifyTikvConfigResponse> {
+        self.client.unary_call(&METHOD_DEBUG_MODIFY_TIKV_CONFIG, req, opt)
+    }
+
+    pub fn modify_tikv_config(&self, req: &super::debugpb::ModifyTikvConfigRequest) -> ::grpcio::Result<super::debugpb::ModifyTikvConfigResponse> {
+        self.modify_tikv_config_opt(req, ::grpcio::CallOption::default())
+    }
+
+    pub fn modify_tikv_config_async_opt(&self, req: &super::debugpb::ModifyTikvConfigRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::debugpb::ModifyTikvConfigResponse>> {
+        self.client.unary_call_async(&METHOD_DEBUG_MODIFY_TIKV_CONFIG, req, opt)
+    }
+
+    pub fn modify_tikv_config_async(&self, req: &super::debugpb::ModifyTikvConfigRequest) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::debugpb::ModifyTikvConfigResponse>> {
+        self.modify_tikv_config_async_opt(req, ::grpcio::CallOption::default())
+    }
     pub fn spawn<F>(&self, f: F) where F: ::futures::Future<Item = (), Error = ()> + Send + 'static {
         self.client.spawn(f)
     }
@@ -290,6 +313,7 @@ pub trait Debug {
     fn list_fail_points(&self, ctx: ::grpcio::RpcContext, req: super::debugpb::ListFailPointsRequest, sink: ::grpcio::UnarySink<super::debugpb::ListFailPointsResponse>);
     fn get_metrics(&self, ctx: ::grpcio::RpcContext, req: super::debugpb::GetMetricsRequest, sink: ::grpcio::UnarySink<super::debugpb::GetMetricsResponse>);
     fn check_region_consistency(&self, ctx: ::grpcio::RpcContext, req: super::debugpb::RegionConsistencyCheckRequest, sink: ::grpcio::UnarySink<super::debugpb::RegionConsistencyCheckResponse>);
+    fn modify_tikv_config(&self, ctx: ::grpcio::RpcContext, req: super::debugpb::ModifyTikvConfigRequest, sink: ::grpcio::UnarySink<super::debugpb::ModifyTikvConfigResponse>);
 }
 
 pub fn create_debug<S: Debug + Send + Clone + 'static>(s: S) -> ::grpcio::Service {
@@ -337,6 +361,10 @@ pub fn create_debug<S: Debug + Send + Clone + 'static>(s: S) -> ::grpcio::Servic
     let instance = s.clone();
     builder = builder.add_unary_handler(&METHOD_DEBUG_CHECK_REGION_CONSISTENCY, move |ctx, req, resp| {
         instance.check_region_consistency(ctx, req, resp)
+    });
+    let instance = s.clone();
+    builder = builder.add_unary_handler(&METHOD_DEBUG_MODIFY_TIKV_CONFIG, move |ctx, req, resp| {
+        instance.modify_tikv_config(ctx, req, resp)
     });
     builder.build()
 }
