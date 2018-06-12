30cfc061b67a6f66dc9c01ff3ea97a243b581cc1
Jay Lee <busyjaylee@gmail.com>
2018-06-12 12:31:23 +0800

    Revert "upgrade protobuf to ~2.0 and grpcio to 0.3. (#269)"
    This reverts commit 32ceddfc72711708ed314a3cbc6c063b4d8fbfd6.

--- generate_rust.sh
+++ generate_rust.sh
@@ -63,19 +63,4 @@ done
 mv ${LIB_RS} lib.rs
 pop
 
-# Use the old way to read protobuf enums.
-# TODO: Remove this once stepancheg/rust-protobuf#233 is resolved.
-for f in src/*; do
-python <<EOF
-import re
-with open("$f") as reader:
-    src = reader.read()
-
-res = re.sub('::protobuf::rt::read_proto3_enum_with_unknown_fields_into\(([^,]+), ([^,]+), &mut ([^,]+), [^\)]+\)\?', 'if \\\\1 == ::protobuf::wire_format::WireTypeVarint {\\\\3 = \\\\2.read_enum()?;} else { return ::std::result::Result::Err(::protobuf::rt::unexpected_wire_type(wire_type)); }', src)
-
-with open("$f", "w") as writer:
-    writer.write(res)
-EOF
-done
-
 cargo build
