ab7debc182d9c536775b3adde7e35716f10bc0b6
Nick Cameron <nrc@ncameron.org>
2019-03-05 18:57:42 +1300

    rust: use features to support Prost or rust-protobuf  (#349)
    * rust: use features to support Prost or rust-protobuf
    
    * Rust: remove unnecessary `extern crate`s
    
    * Use features in generated code
    
    * Rename the feature flags and make code generation opt-in
    
    * Refactor build script
    
    As suggested in https://github.com/pingcap/raft-rs/pull/175
    
    * Use kvproto-build crate
    
    * Change feature names to match GRPC-rs
    
    Also removes use of features in build.rs and use cfg! instead of env var.
    
    * Use published protobuf-build

--- build.rs
+++ build.rs
@@ -11,16 +11,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use regex::Regex;
+use protobuf_build::*;
+use std::fs::read_dir;
 use std::fs::File;
-use std::fs::{read_dir, remove_file};
-use std::io::{Read, Write};
-use std::process::Command;
-use std::str;
+use std::io::Write;
 
 fn main() {
+    // This build script creates files in the `src` directory. Since that is
+    // outside Cargo's OUT_DIR it will cause an error when this crate is used
+    // as a dependency. Therefore, the user must opt-in to regenerating the
+    // Rust files.
+    if !cfg!(feature = "regenerate") {
+        println!("cargo:rerun-if-changed=build.rs");
+        return;
+    }
+
     check_protoc_version();
-    let _ = remove_file("src/lib.rs");
 
     let file_names: Vec<_> = read_dir("proto")
         .expect("Couldn't read proto directory")
@@ -37,97 +43,22 @@ fn main() {
         println!("cargo:rerun-if-changed={}", f);
     }
 
-    generate_rust_files(file_names);
+    // Generate rust-protobuf files.
+    generate_protobuf_files(&file_names, "src/protobuf");
 
-    let mut mod_names: Vec<_> = read_dir("src")
-        .expect("Couldn't read src directory")
-        .filter_map(|e| {
-            let file_name = e.expect("Couldn't list file").file_name();
-            file_name
-                .to_string_lossy()
-                .split(".rs")
-                .next()
-                .map(|n| n.to_owned())
-        })
+    let mod_names = module_names_for_dir("src/protobuf");
+
+    let out_file_names: Vec<_> = mod_names
+        .iter()
+        .map(|m| format!("src/protobuf/{}.rs", m))
         .collect();
-    mod_names.sort();
-
-    replace_read_unknown_fields(&mod_names);
-    generate_lib_rs(&mod_names);
+    let out_file_names: Vec<_> = out_file_names.iter().map(|f| &**f).collect();
+    replace_read_unknown_fields(&out_file_names);
+    generate_protobuf_rs(&mod_names);
 }
 
-fn check_protoc_version() {
-    let ver_re = Regex::new(r"([0-9]+)\.([0-9]+)\.[0-9]").unwrap();
-    let ver = Command::new("protoc")
-        .arg("--version")
-        .output()
-        .expect("Program `protoc` not installed (is it in PATH?).");
-    let caps = ver_re
-        .captures(str::from_utf8(&ver.stdout).unwrap())
-        .unwrap();
-    let major = caps.get(1).unwrap().as_str().parse::<i16>().unwrap();
-    let minor = caps.get(2).unwrap().as_str().parse::<i16>().unwrap();
-    if major == 3 && minor < 1 || major < 3 {
-        panic!(
-            "Invalid version of protoc (required 3.1.x, get {}.{}.x).",
-            major, minor,
-        );
-    }
-}
-
-fn generate_rust_files(file_names: Vec<&str>) {
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src",
-        input: &file_names,
-        includes: &["proto", "include"],
-        customize: protoc_rust::Customize {
-            ..Default::default()
-        },
-    })
-    .unwrap();
-
-    protoc_grpcio::compile_grpc_protos(file_names, &["proto", "include"], "src").unwrap();
-}
-
-// Use the old way to read protobuf enums.
-// FIXME: Remove this once stepancheg/rust-protobuf#233 is resolved.
-fn replace_read_unknown_fields(mod_names: &[String]) {
-    let regex =
-        Regex::new(r"::protobuf::rt::read_proto3_enum_with_unknown_fields_into\(([^,]+), ([^,]+), &mut ([^,]+), [^\)]+\)\?").unwrap();
-    for mod_name in mod_names {
-        let file_name = &format!("src/{}.rs", mod_name);
-
-        let mut text = String::new();
-        {
-            let mut f = File::open(file_name).unwrap();
-            f.read_to_string(&mut text)
-                .expect("Couldn't read source file");
-        }
-
-        let text = regex.replace_all(
-            &text,
-            "if $1 == ::protobuf::wire_format::WireTypeVarint {\
-                $3 = $2.read_enum()?;\
-             } else {\
-                return ::std::result::Result::Err(::protobuf::rt::unexpected_wire_type(wire_type));\
-             }",
-        );
-        let mut out = File::create(file_name).unwrap();
-        out.write_all(text.as_bytes())
-            .expect("Could not write source file");
-    }
-}
-
-fn generate_lib_rs(mod_names: &[String]) {
-    let mut text = r"extern crate futures;
-extern crate grpcio;
-extern crate protobuf;
-extern crate raft;
-
-use raft::eraftpb;
-
-"
-    .to_owned();
+fn generate_protobuf_rs(mod_names: &[String]) {
+    let mut text = "use raft::eraftpb;\n\n".to_owned();
 
     for mod_name in mod_names {
         text.push_str("pub mod ");
@@ -135,7 +66,7 @@ use raft::eraftpb;
         text.push_str(";\n");
     }
 
-    let mut lib = File::create("src/lib.rs").expect("Could not create lib.rs");
+    let mut lib = File::create("src/protobuf.rs").expect("Could not create protobuf.rs");
     lib.write_all(text.as_bytes())
-        .expect("Could not write lib.rs");
+        .expect("Could not write protobuf.rs");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,5 @@
-extern crate futures;
-extern crate grpcio;
-extern crate protobuf;
-extern crate raft;
+#[cfg(lib_rust_protobuf)]
+pub use crate::protobuf::*;
 
-use raft::eraftpb;
-
-pub mod coprocessor;
-pub mod debugpb;
-pub mod debugpb_grpc;
-pub mod errorpb;
-pub mod import_kvpb;
-pub mod import_kvpb_grpc;
-pub mod import_sstpb;
-pub mod import_sstpb_grpc;
-pub mod kvrpcpb;
-pub mod metapb;
-pub mod pdpb;
-pub mod pdpb_grpc;
-pub mod raft_cmdpb;
-pub mod raft_serverpb;
-pub mod tikvpb;
-pub mod tikvpb_grpc;
+#[cfg(lib_prost)]
+pub use crate::prost::*;
