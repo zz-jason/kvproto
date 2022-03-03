f050e5b625ef3e4b35dd4f8dbfd5349e236e3b76
disksing <i@disksing.com>
2022-03-03 17:32:52 +0800

    scripts: improve sed version detect (#875)
    Signed-off-by: disksing <i@disksing.com>

--- scripts/common.sh
+++ scripts/common.sh
@@ -10,10 +10,11 @@ function pop() {
 
 function sed_inplace()
 {
-	if [ `uname` == "Darwin" ]; then
-		sed -i '' "$@"
-	else
+	# bsd sed does not support --version.
+	if `sed --version > /dev/null 2>&1`; then
 		sed -i "$@"
+	else
+		sed -i '' "$@"
 	fi
 }
 
